005cdfd0f7e3ffff946595c8118a7d92bc8051c0
Stefan-Serhii <stefan770@ukr.net>
2021-12-10 22:38:34 +0200

    lesson-20: Добавлен NavLink

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
+import {menuItems} from "./serverFile";
+import {logo} from "./serverFile";
 
 const Navbar = () => {
     return (
@@ -8,25 +11,17 @@ const Navbar = () => {
                 <div className={s.navBlock}>
                     <div className={s.flexBlockSpaceBetween}>
                         <div>
-                            <h2>LOGO</h2>
+                            <h2>{logo}</h2>
                         </div>
                         <div className={s.menuBlock}>
                             <ul className={s.menu}>
-                                <li className={s.menuItem}>
-                                    <a href='/'>Profile</a>
-                                </li>
-                                <li className={s.menuItem}>
-                                    <a href='/dialogs'>Messages</a>
-                                </li>
-                                <li className={s.menuItem}>
-                                    <a href='news'>News</a>
-                                </li>
-                                <li className={s.menuItem}>
-                                    <a href='music'>Music</a>
-                                </li>
-                                <li className={s.menuItem}>
-                                    <a href='settings'>Settings</a>
-                                </li>
+                                {menuItems.map(item => (
+                                    <li className={s.menuItem}>
+                                        <NavLink to={item.to} className={navData => navData.isActive && s.activeLink}>
+                                            {item.item}
+                                        </NavLink>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
---
+++ src/components/navbar/serverFile.js
@@ -0,0 +1,9 @@
+export let menuItems = [
+    {id: 1, item: 'Profile', to: '/'},
+    {id: 2, item: 'Messages', to: '/dialogs'},
+    {id: 3, item: 'News', to: '/news'},
+    {id: 4, item: 'Music', to: '/music'},
+    {id: 5, item: 'Settings', to: '/settings'},
+]
+
+export let logo = 'Logo'
